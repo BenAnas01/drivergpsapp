887870174e57f6a3918811983174e82c783f2a83
= <=>
2024-09-14 01:53:33 +0100

    Driver Gps App commit 1

---
+++ src/main/java/org/example/DriverController/DriverControllerApi.java
@@ -0,0 +1,20 @@
+package org.example.DriverController;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ResourceBundle;
+
+@RestController
+@RequestMapping("/api/driver")
+public class DriverControllerApi {
+
+    @GetMapping("/getall")
+    public ResponseEntity<?> getallDrivers(){
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("ok");
+    }
+}
